744374c46bee5942e8fc46f701bd2ba6fa116fe5
Violetta Kegeles <46904779+vkegeles@users.noreply.github.com>
2021-01-21 23:13:20 +0200

    rotation, delete, change Image to function component, WIP on change the size of window
    1.Added rotation functionality for image button + ease animation
    2.Added delete functionality for image button
    3.Move image state to parent component
    4.Change Image from class to function
    5.Size of image is function instead state, calculated from width
    6.Image size is changing on resize, but first render return another width(WIP)

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import Image from '../Image';
 import './Gallery.scss';
+const ROTATE_ANGLE = 90;
+const TARGET_SIZE = 200;
+
 
 class Gallery extends React.Component {
   static propTypes = {
@@ -13,11 +16,17 @@ class Gallery extends React.Component {
     super(props);
     this.state = {
       images: [],
-      galleryWidth: this.getGalleryWidth()
+      galleryWidth: 0
     };
   }
 
-  getGalleryWidth(){
+  getImageSize() {
+    const { galleryWidth } = this.state;
+    const imagesPerRow = Math.floor(galleryWidth / TARGET_SIZE);
+    return (galleryWidth / imagesPerRow);
+  }
+
+  getGalleryWidth() {
     try {
       return document.body.clientWidth;
     } catch (e) {
@@ -40,17 +49,28 @@ class Gallery extends React.Component {
           res.photos.photo &&
           res.photos.photo.length > 0
         ) {
-          this.setState({images: res.photos.photo});
+          const imagesArr = res.photos.photo.map(photo => Object.assign({ ...photo, rotate: 0 }));
+          this.setState({ images: imagesArr });
         }
       });
   }
 
   componentDidMount() {
-    this.getImages(this.props.tag);
+    this.setState({
+      galleryWidth: document.body.clientWidth
+    }); this.getImages(this.props.tag);
+    window.addEventListener('resize', this.updateWidth);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWidth);
+  }
+
+
+  updateWidth = () => {
     this.setState({
       galleryWidth: document.body.clientWidth
     });
-  }
+  };
 
   componentWillReceiveProps(props) {
     this.getImages(props.tag);
@@ -60,10 +80,25 @@ class Gallery extends React.Component {
     return (
       <div className="gallery-root">
         {this.state.images.map(dto => {
-          return <Image key={'image-' + dto.id} dto={dto} galleryWidth={this.state.galleryWidth}/>;
+          return <Image key={'image-' + dto.id} dto={dto} size={this.getImageSize()} onRotate={this.handleRotate} onDelete={this.handleDelete} onExpand={this.handleExpand} />;
         })}
       </div>
     );
+  }
+
+  handleRotate = (image) => {
+    const images = [...this.state.images];
+    const index = images.indexOf(image);
+    images[index].rotate = (images[index].rotate + ROTATE_ANGLE);
+    this.setState({ images });
+  }
+
+  handleDelete = (image) => {
+    const images = this.state.images.filter(dto => dto.id !== image.id);
+    this.setState({ images });
+  }
+
+  handleExpand = (image) => {
   }
 }
 
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -3,56 +3,37 @@ import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
 import './Image.scss';
 
-class Image extends React.Component {
-  static propTypes = {
-    dto: PropTypes.object,
-    galleryWidth: PropTypes.number
-  };
+function Image(props) {
 
-  constructor(props) {
-    super(props);
-    this.calcImageSize = this.calcImageSize.bind(this);
-    this.state = {
-      size: 200
-    };
-  }
-
-  calcImageSize() {
-    const {galleryWidth} = this.props;
-    const targetSize = 200;
-    const imagesPerRow = Math.round(galleryWidth / targetSize);
-    const size = (galleryWidth / imagesPerRow);
-    this.setState({
-      size
-    });
-  }
-
-  componentDidMount() {
-    this.calcImageSize();
-  }
-
-  urlFromDto(dto) {
+  function urlFromDto(dto) {
     return `https://farm${dto.farm}.staticflickr.com/${dto.server}/${dto.id}_${dto.secret}.jpg`;
   }
 
-  render() {
-    return (
-      <div
-        className="image-root"
-        style={{
-          backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
-          width: this.state.size + 'px',
-          height: this.state.size + 'px'
-        }}
-        >
-        <div>
-          <FontAwesome className="image-icon" name="sync-alt" title="rotate"/>
-          <FontAwesome className="image-icon" name="trash-alt" title="delete"/>
-          <FontAwesome className="image-icon" name="expand" title="expand"/>
-        </div>
+  return (
+    <div
+      className="image-root"
+      style={{
+        backgroundImage: `url(${urlFromDto(props.dto)})`,
+        width: props.size + 'px',
+        height: props.size + 'px',
+        transform: `rotate(${props.dto.rotate}deg)`
+      }}
+    >
+      <div style={{
+        transform: `rotate(${-props.dto.rotate}deg)` //reverse rotation for child
+      }}>
+        <FontAwesome className="image-icon" name="sync-alt" title="rotate" onClick={() => props.onRotate(props.dto)} />
+        <FontAwesome className="image-icon" name="trash-alt" title="delete" onClick={() => props.onDelete(props.dto)} />
+        <FontAwesome className="image-icon" name="expand" title="expand" onClick={() => props.onExpand(props.dto)} />
       </div>
-    );
-  }
+    </div>
+  );
+
 }
 
+
+Image.propTypes = {
+  dto: PropTypes.object,
+  size: PropTypes.number
+}
 export default Image;
